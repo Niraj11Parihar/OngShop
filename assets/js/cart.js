548e023bdc10a8011014a49a8cddf81249aa99b4
Niraj Parihar <nirajp201icloud.com@gmail.com>
2024-11-23 16:03:05 +0530

    cartDesign

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,30 +1,34 @@
 var cart = JSON.parse(localStorage.getItem('cart')) || [];
-var total = 0;
 var totalCount = 0;
 
 function updateCartDisplay() {
   var tableBody = document.getElementById('table-cart');
   var tr = "";
+  var subtotal = 0;
 
   for (var i = 0; i < cart.length; i++) {
     var qty = parseInt(cart[i].qty);
-    var price = parseInt(cart[i].price) ;
+    var price = parseFloat(cart[i].price);
+    var productTotal = qty * price;
+    subtotal += productTotal;
+
     tr += `
       <tr>
         <td>${cart[i].name}</td>
-        <td>${cart[i].price}</td>
-        <td>${cart[i].qty}</td>
-        <td>${(cart[i].qty * cart[i].price).toFixed(2)}</td>
+        <td>$${price.toFixed(2)}</td>
         <td>
-          <button class="del" onclick="deleteProduct(${i})">DELETE</button>
+          <input type="number" min="1" class="form-control text-center" value="${qty}" 
+          onchange="updateQuantity(${i}, this.value)">
+        </td>
+        <td>$${productTotal.toFixed(2)}</td>
+        <td>
+          <button class="btn btn-danger btn-sm" onclick="deleteProduct(${i})">Delete</button>
         </td> 
       </tr>`;
-
-    total += qty * price;
   }
 
   tableBody.innerHTML = tr;
-  // updateTotal();
+  updateSubtotal(subtotal);
 }
 
 function deleteProduct(index) {
@@ -34,14 +38,21 @@ function deleteProduct(index) {
   viewCart();
 }
 
-function viewCart() {
-  totalCount = cart.length;
-  document.getElementById("count").innerHTML = totalCount;
+function updateQuantity(index, newQty) {
+  if (newQty <= 0) return;
+  cart[index].qty = newQty;
+  localStorage.setItem('cart', JSON.stringify(cart));
+  updateCartDisplay();
 }
 
-// function updateTotal() {
-//   document.getElementById("total").innerHTML = total.toFixed(2);
-// }
+function updateSubtotal(subtotal) {
+  document.getElementById('subtotal').innerText = `$${subtotal.toFixed(2)}`;
+}
+
+function viewCart() {
+  totalCount = cart.length;
+  document.getElementById("count").innerHTML = totalCount || 0;
+}
 
 viewCart();
 updateCartDisplay();
